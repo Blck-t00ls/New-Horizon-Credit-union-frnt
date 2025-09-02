e7c5c7af786e9702e7ecc5c8b8da8a20289ac810
BLCK-HT-T00L$ <supprtmailer.notification@gmail.com>
2025-09-02 12:52:54 +0000

    update

--- api/server.js
+++ api/server.js
@@ -1,11 +1,3 @@
-dotenv.config();
-
-
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { Pool } = require('pg');
-dotenv.config();
 
 const express = require('express');
 const cors = require('cors');
